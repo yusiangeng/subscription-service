8dfd77b15bfe5e293d362da3e6b8f3d17816aec6
yusiangeng <engyusiang@icloud.com>
2022-09-17 16:42:06 +0800

    Add Makefile

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
 const webPort = "80"
 
 func main() {
 	// connect to db
+	db := initDB()
+	db.Ping()
 
 	// sessions
 
@@ -14,4 +27,50 @@ func main() {
 	// application config
 
 	// email
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("could not connect to database")
+	}
+
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("postgres not yet ready...")
+		} else {
+			log.Println("connected to database")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		time.Sleep(1 * time.Second)
+		counts++
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
